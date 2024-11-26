e597b7cab32d4105ab4f5949885fc884865699c7
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-11-26 13:25:51 -0600

    175. Typing Animations, Timers and For Loops

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
-    }
-    
-
+	
+	@IBOutlet weak var titleLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		titleLabel.text = ""
+		var charIndex = 0
+		let titleText = "⚡️FlashChat"
+		for letter in titleText {
+			print(charIndex)
+			print(letter)
+			Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { _ in
+				self.titleLabel.text?.append(letter)
+				
+			}
+			charIndex += 1
+		}
+	}
 }
