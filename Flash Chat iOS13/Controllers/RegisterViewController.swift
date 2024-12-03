d5c01c876dceefdbaf94cf84ddaf0d7c3016ee0a
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-02 18:30:09 -0600

    187. Logging Out Users

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -11,9 +11,11 @@ import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+	// MARK: - Outlets
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+	// MARK: - Actions
     @IBAction func registerPressed(_ sender: UIButton) {
 		if let email = emailTextfield.text, let password = passwordTextfield.text {
 			Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
