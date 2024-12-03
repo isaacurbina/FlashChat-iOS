165fa5c2253823fad46408d6e349e453ea97e49b
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-03 15:22:08 -0600

    189. How to use a UITableView and Create a Message Model

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -11,6 +11,12 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
 
+	var messages: [Message] = [
+		Message(sender: "1@2.com", body: "Hey!"),
+		Message(sender: "1@2.com", body: "Hello!"),
+		Message(sender: "1@2.com", body: "What's up?")
+	]
+	
 	// MARK: - Outlets
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
@@ -19,6 +25,7 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 		title = Constants.appName
+		tableView.dataSource = self
 		navigationItem.hidesBackButton = true
     }
     
@@ -34,5 +41,18 @@ class ChatViewController: UIViewController {
 		} catch let signOutError as NSError {
 		  print("Error signing out: %@", signOutError)
 		}
+	}
+}
+
+// MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return messages.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+		cell.textLabel?.text = messages[indexPath.row].body
+		return cell
 	}
 }
