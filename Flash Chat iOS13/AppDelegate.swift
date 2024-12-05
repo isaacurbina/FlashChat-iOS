24701067eb645e8a4cc12c05489349e17d24b8ec
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-05 15:39:58 -0600

    192. Database setup and Saving Data to Firestore (#12)
    * 192. Database setup and Saving Data to Firestore
    
    * 192. Database setup and Saving Data to Firestore

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -9,14 +9,15 @@
 import UIKit
 import FirebaseCore
 import FirebaseAuth
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		FirebaseApp.configure()
+		let db = Firestore.firestore()
+		print(db)
         return true
     }
 
