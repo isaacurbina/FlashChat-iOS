57ce31a1d18f09b7ed3abfa84df218edfcbd9dd4
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-06 15:34:17 -0600

    197. Finishing Touches: UI and UX Improvements (#17)

--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -14,6 +14,8 @@ class MessageCell: UITableViewCell {
 	@IBOutlet weak var messageBubble: UIView!
 	@IBOutlet weak var label: UILabel!
 	@IBOutlet weak var rightImageView: UIImageView!
+	@IBOutlet weak var leftImageView: UIImageView!
+	
 	
 	// MARK: - UITableViewCell
 	override func awakeFromNib() {
